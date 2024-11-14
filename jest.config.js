7b7d539448c59f400669af4e54014a96f273bb93
karthikmeda <mkarthikmeda@gmail.com>
2024-11-14 17:25:59 +0530

    v1

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,35 @@
-module.exports = {
-    preset: 'ts-jest/presets/default-esm', // If TypeScript is used
-    extensionsToTreatAsEsm: ['.js'],
+// module.exports = {
+//     preset: 'ts-jest/presets/default-esm', // If TypeScript is used
+//     extensionsToTreatAsEsm: ['.ts', '.tsx', '.js', '.jsx'],
+//     transform: {
+//       '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+//     },
+//     transformIgnorePatterns: [
+//       '/node_modules/(?!axios)/' // This will transpile axios as well
+//     ],
+//     // Other Jest configuration options can go here
+//     moduleNameMapper: {
+//         '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js', // Add this line
+//       },
+//   };
+ 
+  
+
+  module.exports = {
     transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+      '^.+\\.[t|j]sx?$': 'babel-jest', // Use Babel for JS/JSX transformation
     },
     transformIgnorePatterns: [
-      '/node_modules/(?!axios)/' // This will transpile axios as well
+      '/node_modules/(?!axios)/', // Include axios in the transformation
     ],
-    // Other Jest configuration options can go here
+    extensionsToTreatAsEsm: ['.js', '.jsx', '.ts', '.tsx'], // Treat JS/TS/JSX/TSX files as ESM
+    moduleNameMapper: {
+      '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js', // Mock CSS imports
+    },
+    globals: {
+      'ts-jest': {
+        useESM: true, // Enable ESM support for TypeScript if used
+      },
+    },
   };
- 
+  
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App.js';
+// import { render, screen } from '@testing-library/react';
+// import App from './App.js';
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders Spotify Music heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/spotify music/i);
+  expect(headingElement).toBeInTheDocument();
 });
